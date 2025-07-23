52d0cb2ec4ffa0e9a720f452f1087cf169627e8f
ryougi-shiky <ryougikalan@gmail.com>
2025-07-23 19:52:44 +0800

    feat: add LocalStack deployment and configuration scripts

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.65"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  endpoints {
+    ec2 = "http://localhost:4566"
+  }
+}
+
+resource "aws_instance" "test" {
+  ami                         = "ami-12345678" # dummy
+  instance_type               = "t2.micro"
+}
