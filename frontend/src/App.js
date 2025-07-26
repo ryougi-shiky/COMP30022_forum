16f51a84c7c62bb6ec3c7ec824a5cabc28571c77
RyougiShiki <53469345+ryougi-shiky@users.noreply.github.com>
2025-07-26 11:33:59 +0800

    refactor: remove socket.io dependencies and Messenger component (#138)
    * refactor: remove socket.io integration from Messenger component and related configurations
    
    # Conflicts:
    #	frontend/package-lock.json
    
    * chore: remove unused onlineUsers state from Messenger component
    
    * chore: remove Messenger component
    
    * chore: remove Messenger route from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile"
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import Messenger from "./pages/messenger/Messenger";
 import Moments from "./pages/moments/Moments";
 import HealthCheck from "./pages/healthcheck/HealthCheck";
 
@@ -28,20 +27,9 @@ function App() {
             <Route exact path="/login" element={(user && cookies.user) ? <Navigate to='/' /> : <Login />}></Route>
             <Route exact path="/register" element={cookies.user ? <Navigate to='/' /> : <Register />}></Route>
             <Route path="/profile/:username" element={user ? <Profile /> : <Navigate to='/login' />}></Route>
-            <Route exact path="/messenger" element= {user ? <Messenger /> : <Navigate to='/' /> }></Route>
             <Route exact path="/moments" element= {user ? <Moments /> : <Navigate to='/' /> }></Route>
             <Route exact path="/healthcheck" element= { <HealthCheck /> }></Route>
-            {/* <Route exact path="/messenger" element= { <Messenger /> }></Route> */}
-
-            {/* <Route path="/login" element={<Login />}></Route>
-            <Route path="/recordlist" element={<RecordList />}></Route>
-            <Route path="/edit/:id" element={<Edit />}></Route>
-            <Route path="/register" element={<Register />}></Route>
-            <Route path="/login/user/:username" element={<App />}></Route> */}
         </Routes>
-        {/* <Routes>
-            <Route path="*" element={<Error />}></Route>
-        </Routes> */}
     </BrowserRouter>
   )
 }
