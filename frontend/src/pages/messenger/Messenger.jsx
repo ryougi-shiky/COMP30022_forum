a6d4de7bf67d0594ed7dcab21dad20a954f02c3d
ryougi-shiky <ryougikalan@gmail.com>
2025-07-26 11:04:50 +0800

    refactor: remove socket.io integration from Messenger component and related configurations
    # Conflicts:
    #	frontend/package-lock.json

--- frontend/src/pages/messenger/Messenger.jsx
+++ frontend/src/pages/messenger/Messenger.jsx
@@ -8,9 +8,7 @@ import ChatOnline from "../../components/chatOnline/ChatOnline";
 
 import React, { useContext, useState, useEffect, useRef } from "react";
 import axios from "axios";
-import { io } from "socket.io-client";
 
-const socket_server_port = process.env.REACT_APP_SOCKET_SERVER_PORT;
 const backend_url = process.env.REACT_APP_BACKEND_URL;
 
 export default function Messenger() {
@@ -19,51 +17,8 @@ export default function Messenger() {
   const [currentChat, setCurrentChat] = useState();
   const [messages, setMessages] = useState();
   const [newMessage, setNewMessage] = useState("");
-  const [arrivalMessage, setArrivalMessage] = useState(null);
   const [onlineUsers, setOnlineUsers] = useState([]);
-  // const [socket, setSocket] = useState(null);
-  // const socket = useRef(io(`ws://localhost:${process.env.REACT_APP_SOCKET_SERVER_PORT}`));
-  const socket = useRef();
-  // const socket = io(`ws://localhost:${process.env.REACT_APP_SOCKET_SERVER_PORT}`);
   const scrollRef = useRef();
-
-  useEffect(() => {
-    socket.current = io(
-      `ws://localhost:${process.env.REACT_APP_SOCKET_SERVER_PORT}`
-    );
-    socket.current.on("getMessage", (data) => {
-      setArrivalMessage({
-        sender: data.senderId,
-        text: data.text,
-        createdAt: Date.now(),
-      });
-    });
-  }, []);
-
-  useEffect(() => {
-    arrivalMessage &&
-      currentChat?.members.includes(arrivalMessage.senderId) &&
-      setMessages((prev) => [...prev, arrivalMessage]);
-  }, [arrivalMessage, currentChat]);
-
-  useEffect(() => {
-    console.log("connecting to ws://localhost:" + socket_server_port);
-    // setSocket(io('ws://localhost:'+socket_server_port));
-
-    socket.current?.emit("addUser", currentUser._id);
-    socket.current?.on("getUsers", (users) => {
-      console.log("socket users: ", users);
-      setOnlineUsers(
-        currentUser.followings.filter((f) => users.some((u) => u.uid === f))
-      );
-    });
-  }, [currentUser]);
-
-  useEffect(() => {
-    socket.current?.on("welcome", (message) => {
-      console.log("welcome message: ", message);
-    });
-  }, [socket]);
 
   useEffect(() => {
     const getConversation = async () => {
@@ -72,16 +27,12 @@ export default function Messenger() {
           `${backend_url}/conversation/${currentUser._id}`
         );
         setConversations(res.data);
-        // console.log("conversations res: ", res);
-        // console.log("conversations: ", conversations);
       } catch (err) {
         console.log(err);
       }
     };
     getConversation();
   }, [currentUser._id]);
-
-  // console.log("currentChat: ", currentChat);
 
   useEffect(() => {
     const getMessages = async () => {
@@ -96,7 +47,6 @@ export default function Messenger() {
     };
     getMessages();
   }, [currentChat]);
-  console.log("get message: ", messages);
 
   const submitMessage = async (e) => {
     e.preventDefault();
@@ -105,16 +55,6 @@ export default function Messenger() {
       senderId: currentUser._id,
       text: newMessage,
     };
-
-    const receiverId = currentChat.members.find(
-      (member) => member !== currentUser._id
-    );
-
-    socket.current.emit("sendMessage", {
-      senderId: currentUser._id,
-      receiverId,
-      text: newMessage,
-    });
 
     try {
       const res = await axios.post(`${backend_url}/message`, message);
